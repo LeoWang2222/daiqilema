bf7f4b6d49ca8d2fa63697688d6c12c1288a918d
LeoWang2222 <LeoWang2222@users.noreply.github.com>
2026-07-19 21:11:57 +0800

    带齐了吗 v1

---
+++ sw.js
@@ -0,0 +1,30 @@
+var CACHE = 'daiqilema-v1';
+var FILES = ['./', './index.html'];
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(caches.open(CACHE).then(function (c) { return c.addAll(FILES); }));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.map(function (k) {
+        if (k !== CACHE) return caches.delete(k);
+      }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (hit) {
+      return hit || fetch(e.request).then(function (resp) {
+        var copy = resp.clone();
+        caches.open(CACHE).then(function (c) { c.put(e.request, copy); });
+        return resp;
+      }).catch(function () { return hit; });
+    })
+  );
+});
